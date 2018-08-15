4f83e1f62eba9df788e56988995fbb553a8608a6
Carlos Villanueva <prince.vill10@gmail.com>
2018-08-14 21:27:13 -0600

    one
    one

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
 import Weather from './Weather';
+import Axios from 'axios';
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      Ubicacion: '',
-      Temperatur: '',
-      Edad: ''
+      ubication: '',
+      temp: '',
+      humedad: ''
     }
 }
 
+// componentDidMount() {
+//   axios.get(`http://api.openweathermap.org/data/2.5/weather?q='{city}','{country}'&appid='{API_KEY}'&u
+//   nits=metric
+//   `)
+//     .then(res => {
+//       const persons = res.data;
+//       this.setState({ persons   });
+//     })
+// }
+
+
   updateState(){
-    
+    {}
   }
 
 
   render() {
     return (
       <div className="App">
-        <Form myUbicacionProp = {this.state.Ubicacion} />
-        <Weather/>
+        <div className = "flex-container">
+          <div className = "inside">
+          <Form updateState = {this.updateState} />
+          <Weather ubicaction ={this.state.ubicaction} temp = {this.state.temp} humidity = {this.state.humedad}/>
+          </div>
+        </div>
       </div>
     );
   }
